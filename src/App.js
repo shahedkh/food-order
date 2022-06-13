84dabfca80ffe6cca386dd4377c7f52d2dfcf12e
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-13 21:09:10 +0300

    add meals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 import Header from "./component/layout/Header";
+import Meals from './component/Meals/Meals'
 
 function App() {
   return (
-    <Fragment ><Header/></Fragment>
+    <Fragment ><Header/>
+    <main><Meals/></main></Fragment>
   );
 }
 
