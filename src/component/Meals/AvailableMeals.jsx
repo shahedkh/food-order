6944fb6af1147501ed3cdbafc4ab1ab493846dac
shahedkh <alkhalailah.shahed@gmail.com>
2022-06-13 21:49:20 +0300

    add meal item

--- src/component/Meals/AvailableMeals.jsx
+++ src/component/Meals/AvailableMeals.jsx
@@ -1,5 +1,5 @@
-// import Card from '../UI/Card';
-// import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -30,21 +30,21 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-//   const mealsList = DUMMY_MEALS.map((meal) => (
-//     <MealItem
-//       key={meal.id}
-//       id={meal.id}
-//       name={meal.name}
-//       description={meal.description}
-//       price={meal.price}
-//     />
-//   ));
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      {/* <Card>
+      <Card>
         <ul>{mealsList}</ul>
-      </Card> */}
+      </Card>
     </section>
   );
 };
